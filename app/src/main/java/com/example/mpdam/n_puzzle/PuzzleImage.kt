bcc94bb1f06aacdd866befa6c0f55c06e9bfe0cd
MeowDevelopment <ameny.ferchichi99@gmail.com>
2023-02-17 22:03:15 +0100

    commited

---
+++ app/src/main/java/com/example/mpdam/n_puzzle/PuzzleImage.kt
@@ -0,0 +1,17 @@
+package com.example.mpdam.n_puzzle
+
+enum class PuzzleImage(val drawableId: Int) {
+
+    Meow(R.drawable.cat2),
+
+  Cats(R.drawable.catgroup),
+
+    Puzzle(R.drawable.puzzle),
+
+    Kitten(R.drawable.cat3),
+
+    Dog(R.drawable.dog3),
+
+
+    CUSTOM(-1)
+}
